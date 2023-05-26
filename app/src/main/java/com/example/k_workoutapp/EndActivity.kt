e779e43ebb9ff982bf106803aedaef7e08827bbb
Kunal_801 <kunalthopte9995@gmail.com>
2023-05-26 11:05:19 +0530

    Added author in EndActivity

--- app/src/main/java/com/example/k_workoutapp/EndActivity.kt
+++ app/src/main/java/com/example/k_workoutapp/EndActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.k_workoutapp.databinding.ActivityEndBinding
 
+/**
+ * @author Kunal
+ */
 class EndActivity : AppCompatActivity() {
 
     private var binding : ActivityEndBinding?= null
